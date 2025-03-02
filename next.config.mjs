57cbeb861cf49ab86198196f602d2b3ed3f148e3
pirocheto <pirocheto@gmail.com>
2025-03-02 15:10:53 +0100

    fix basepath

--- next.config.mjs
+++ next.config.mjs
@@ -18,7 +18,7 @@ const config = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "data-engineering-website/",
+  basePath: "/data-engineering-website",
 
   /**
    * Disable server-based image optimization. Next.js does not support
