980fbcc2477afe3af91496462b9d4ac019bf8658
pirocheto <pirocheto@gmail.com>
2025-03-02 16:33:43 +0100

    change BASE_PATH

--- next.config.mjs
+++ next.config.mjs
@@ -19,7 +19,6 @@ const config = {
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
   basePath: process.env.BASE_PATH || "",
-  assetPrefix: process.env.BASE_PATH || "",
 
   /**
    * Disable server-based image optimization. Next.js does not support
