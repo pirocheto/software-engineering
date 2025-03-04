f8d1da52da01d76850daddfc58525a5c5b2162a2
pirocheto <pirocheto@gmail.com>
2025-03-04 20:01:24 +0100

    fix color

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -18,16 +18,20 @@ const docsOptions: DocsLayoutProps = {
         const meta = source.getNodeMeta(node)
         if (!meta) return option
 
-        const color = `var(--${meta.file.dirname}-color, var(--color-fd-foreground))`
+        const dirname = meta.file.dirname
+        const color = `var(--${dirname}-color, var(--color-fd-foreground))`
 
         return {
           ...option,
+          props: {
+            // className: `${dirname}-hover`,
+          },
           icon: (
             <div
-              className="rounded-md p-1 shadow-lg ring-2 [&_svg]:size-5"
+              className="rounded-md p-1 shadow-lg ring-2 [&_svg]:size-5 "
               style={
                 {
-                  color,
+                  color: color,
                   border: `1px solid color-mix(in oklab, ${color} 50%, transparent)`,
                   "--tw-ring-color": `color-mix(in oklab, ${color} 20%, transparent)`,
                 } as object
