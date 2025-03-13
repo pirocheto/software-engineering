3703f13d9b705ccddb4a0a146409b86af1f8d37c
pirocheto <pirocheto@gmail.com>
2025-03-13 17:52:43 +0100

    add types

--- components/callout-exemple.tsx
+++ components/callout-exemple.tsx
@@ -3,14 +3,12 @@ import { MdRocketLaunch } from "react-icons/md"
 import { ReactNode } from "react"
 
 const CalloutExemple = ({ children }: { children: ReactNode }) => {
-  const purple = "purple-600"
-
   return (
     <Callout
       type="info"
       title="Exemple"
-      className={`border-${purple} border-1`}
-      icon={<MdRocketLaunch className={`fill-${purple}`} />}
+      className="border-purple-600 border-2"
+      icon={<MdRocketLaunch className="fill-purple-600" />}
     >
       {children}
     </Callout>
